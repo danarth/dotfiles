a3a2bde31f5f51bfa1334b30c31b1467e89f6132
Dan Arthur <daniel.arthur@cazenovecapital.com>
2024-02-21 15:13:47 +0000

    Add python specific key-maps

--- nvim/lua/keymap/dap.lua
+++ nvim/lua/keymap/dap.lua
@@ -3,6 +3,7 @@ local nmap = keymap.nmap
 local silent, noremap = keymap.silent, keymap.noremap
 local cmd = keymap.cmd
 local opts = keymap.new_opts
+local api = vim.api
 
 nmap({
   { '<leader>xr', cmd('Telescope dap configurations'), opts("Debug: Run Something", noremap, silent) },
@@ -10,4 +11,17 @@ nmap({
   { '<leader>xb', cmd('DapToggleBreakpoint'), opts("Debug: Toggle Breakpoint", noremap, silent) },
   { '<leader>xl', cmd('DapContinue'), opts("Debug: Continue", noremap, silent) },
   { '<leader>xf', cmd('DapToggleRepl'), opts("Debug: Toggle Console", noremap, silent) }
+})
+
+local dap_python_keymap = api.nvim_create_augroup("dap_python_keymap", { clear = true})
+api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  group = dap_python_keymap,
+  callback = function()
+    local dap_python = require('dap-python')
+    nmap({
+      { '<leader>xt', dap_python.test_method, opts("Debug: Nearest Test", noremap, silent) },
+      { '<leader>xc', dap_python.test_class, opts("Debug: Nearest Class", noremap, silent) },
+    })
+  end
 })
