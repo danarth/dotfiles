04907db9d6a038e06f3a63b3a0d824e7d5e351bc
Dan Arthur <daniel.arthur@cazenovecapital.com>
2024-03-25 14:14:10 +0000

    Bring back hop

--- nvim/lua/modules/editor/package.lua
+++ nvim/lua/modules/editor/package.lua
@@ -31,9 +31,10 @@ package({
 })
 
 package({
-  'folke/flash.nvim',
-  event = 'VeryLazy',
-  opts = {},
+  'smoka7/hop.nvim',
+  config = function()
+    require('hop').setup({})
+  end,
 })
 
 package({
