d55b2737790ed7a79dc04e7555f40368fc465503
Daniel Arthur <me@danielarthur.uk>
2024-09-15 22:03:44 +0100

    more editor plugins

--- nvim/lua/modules/editor/package.lua
+++ nvim/lua/modules/editor/package.lua
@@ -21,6 +21,24 @@ package({
 })
 
 package({
+  'yamatsum/nvim-cursorline',
+  config = function ()
+    require('nvim-cursorline').setup {
+      cursorline = {
+        enable = true,
+        timeout = 100,
+        number = false,
+      },
+      cursorword = {
+        enable = true,
+        min_length = 3,
+        hl = { underline = true },
+      }
+    }
+  end
+})
+
+package({
   'nvim-treesitter/nvim-treesitter',
   event = 'BufRead',
   run = ':TSUpdate',
