ea3984b441425a6c67ac62a03af76f387b500d5d
Dan Arthur <danarthur6@gmail.com>
2025-04-30 12:57:43 +0100

    feat(nvim): experiment with mcp

--- nvim/lua/modules/ai/package.lua
+++ nvim/lua/modules/ai/package.lua
@@ -29,3 +29,15 @@ package({
     },
   },
 })
+
+package({
+  'ravitemer/mcphub.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  cmd = 'MCPHub',
+  build = 'npm install -g mcp-hub@latest',
+  config = function()
+    require('mcphub').setup()
+  end,
+})
