c406db4d64f23f4058d487cdfc1a2da033f1148b
Dan Arthur <danarthur6@gmail.com>
2025-08-27 10:56:34 +0100

    feat(nvim): better integrate conform

--- nvim/lua/modules/editor/package.lua
+++ nvim/lua/modules/editor/package.lua
@@ -105,5 +105,15 @@ package('unblevable/quick-scope')
 
 package({
   'stevearc/conform.nvim',
-  opts = {},
+  config = function()
+    require('conform').setup({
+      formatters_by_ft = {
+        sql = { 'pg_format' },
+      },
+      default_format_opts = {
+        lsp_format = 'fallback',
+      },
+    })
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
 })
