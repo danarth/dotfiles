9ad534ae1f6cd5080cd54aab04730f915fdc1311
Dan Arthur <danarthur6@gmail.com>
2025-08-27 16:59:23 +0100

    feat(nvim): more textobjects

--- nvim/lua/modules/editor/config.lua
+++ nvim/lua/modules/editor/config.lua
@@ -1,6 +1,7 @@
 local config = {}
 
 function config.nvim_treesitter()
+  ---@diagnostic disable-next-line: missing-fields
   require('nvim-treesitter.configs').setup({
     autotag = {
       enable = true,
@@ -20,6 +21,14 @@ function config.nvim_treesitter()
           ['aa'] = '@parameter.outer',
           ['ia'] = '@parameter.inner',
           ['as'] = '@statement.outer',
+          ['al'] = '@loop.outer',
+          ['il'] = '@loop.inner',
+          ['ai'] = '@conditional.outer',
+          ['ii'] = '@conditional.inner',
+          ['=h'] = '@assignment.lhs',
+          ['=l'] = '@assignment.rhs',
+          ['a='] = '@assignment.outer',
+          ['i='] = '@assignment.inner',
         },
       },
       swap = {
@@ -29,6 +38,38 @@ function config.nvim_treesitter()
         },
         swap_previous = {
           ['<leader>sA'] = '@parameter.inner',
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true,
+        goto_next_start = {
+          [']m'] = '@function.outer',
+          [']c'] = '@class.outer',
+          [']l'] = '@loop.outer',
+          [']i'] = '@conditional.outer',
+          [']r'] = '@return.outer',
+        },
+        goto_next_end = {
+          [']M'] = '@function.outer',
+          [']C'] = '@class.outer',
+          [']L'] = '@loop.outer',
+          [']I'] = '@conditional.outer',
+          [']R'] = '@return.outer',
+        },
+        goto_previous_start = {
+          ['[m'] = '@function.outer',
+          ['[c'] = '@class.outer',
+          ['[l'] = '@loop.outer',
+          ['[i'] = '@conditional.outer',
+          ['[r'] = '@return.outer',
+        },
+        goto_previous_end = {
+          ['[M'] = '@function.outer',
+          ['[C'] = '@class.outer',
+          ['[L'] = '@loop.outer',
+          ['[I'] = '@conditional.outer',
+          ['[R'] = '@return.outer',
         },
       },
     },
