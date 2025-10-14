a310d01379dc2ca0de691b1ba7bc23b2d08a1168
Dan Arthur <danarthur6@gmail.com>
2025-10-14 11:40:55 +0100

    feat(nvim): add boolean to dial.nvim

--- nvim/lua/modules/editor/package.lua
+++ nvim/lua/modules/editor/package.lua
@@ -85,7 +85,23 @@ package({
   end,
 })
 
-package('monaqa/dial.nvim')
+package({
+  'monaqa/dial.nvim',
+  config = function()
+    local augend = require('dial.augend')
+    require('dial.config').augends:register_group({
+      default = {
+        augend.integer.alias.decimal,
+        augend.integer.alias.hex,
+        augend.date.alias['%Y/%m/%d'],
+        augend.date.alias['%Y-%m-%d'],
+        augend.date.alias['%m/%d'],
+        augend.date.alias['%H:%M'],
+        augend.constant.alias.bool,
+      },
+    })
+  end,
+})
 
 package({
   'danymat/neogen',
