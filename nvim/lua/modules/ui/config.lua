2d9043a45a51d66fc2364d88df2b3221d2d1fbaa
Dan Arthur <danarthur6@gmail.com>
2026-02-26 14:22:20 +0000

    feat(nvim): bufferline improvement

--- nvim/lua/modules/ui/config.lua
+++ nvim/lua/modules/ui/config.lua
@@ -21,8 +21,15 @@ function config.nvim_bufferline()
         delay = 50,
         reveal = { 'close' },
       },
-      separator_style = 'slope',
       always_show_bufferline = true,
+      offsets = {
+        {
+          filetype = 'neo-tree',
+          text = 'File Explorer',
+          highlight = 'Directory',
+          text_align = 'center',
+        },
+      },
       get_element_icon = function(element)
         local icon, hl = require('nvim-web-devicons').get_icon_by_filetype(element.filetype, { default = false })
         local custom_map = {
@@ -38,30 +45,6 @@ function config.nvim_bufferline()
       end,
     },
   })
-  -- local Offset = require('bufferline.offset')
-  -- if not Offset.edgy then
-  --   local get = Offset.get
-  --   Offset.get = function()
-  --     if package.loaded.edgy then
-  --       local layout = require('edgy.config').layout
-  --       local ret = { left = '', left_size = 0, right = '', right_size = 0 }
-  --       for _, pos in ipairs({ 'left', 'right' }) do
-  --         local sb = layout[pos]
-  --         if sb and #sb.wins > 0 then
-  --           local title = string.rep(' ', sb.bounds.width) -- Title madde from whitespace
-  --           ret[pos] = '%#EdgyTitle#' .. title .. '%*' .. '%#WinSeparator#│%*'
-  --           ret[pos .. '_size'] = sb.bounds.width
-  --         end
-  --       end
-  --       ret.total_size = ret.left_size + ret.right_size
-  --       if ret.total_size > 0 then
-  --         return ret
-  --       end
-  --     end
-  --     return get()
-  --   end
-  --   Offset.edgy = true
-  -- end
 end
 
 return config
