8bda9731989a7b1323f82e376673e0df3702af27
Arthur, Daniel <daniel.arthur@troweprice.com>
2026-05-29 11:16:51 +0100

    feat(fish): add multicd abbreviation

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
 if set -q NVIM; and command -q nvr
   set -gx GIT_EDITOR "nvr -cc split --remote-wait"
 end
+
+function multicd
+    echo cd (string repeat -n (math (string length -- $argv[1]) - 1) ../)
+end
+abbr --add dotdot --regex '^\.\.+$' --function multicd
